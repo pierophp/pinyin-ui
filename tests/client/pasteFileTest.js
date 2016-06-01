64d580594b7d93f12d4a41784ce1cb6cc008e0d7
Piero Giusti <pierophp@gmail.com>
2016-06-01 17:47:52 -0300

    Fix travis

--- tests/client/pasteFileTest.js
+++ tests/client/pasteFileTest.js
@@ -7,8 +7,14 @@ describe('example test', function () {
     }))
 
     it('should be true', inject(function (filesAPI) {
+        
+        console.log('Start Test');
+        
         var expectParsed = [{'p': '5', 'c': ''},{'p': 'Nà', 'c': '那'}, {'p': 'shíhou', 'c': '时候'}, {'p': 'mángrén', 'c': '盲人'}];
         var result = _filesAPI.parseClipboard('5 Nà 那 shíhou  时候 mángrén 盲人');
         expect(JSON.stringify(expectParsed)).toBe(JSON.stringify(result));
+        
+        console.log('End Test');
+        
     }));
 });
