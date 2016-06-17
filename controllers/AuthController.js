9a5095654b037f5d1f4370c065cdf1294d41d600
Piero Giusti <pierophp@gmail.com>
2016-06-17 14:33:53 -0300

    Fix on Print

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -4,15 +4,21 @@ var passport = require('passport');
 var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
 var env = require('../env');
 
-passport.use(new GoogleStrategy({
-    clientID: env.google_client_id,
-    clientSecret: env.google_client_secret,
-    callbackURL: env.base_url + "/auth/google/callback"
-},
-    function (accessToken, refreshToken, profile, done) {
-        //return done(err, user);
-    }
-));
+
+if (env.google_client_id && env.google_client_secret) {
+
+    passport.use(new GoogleStrategy({
+        clientID: env.google_client_id,
+        clientSecret: env.google_client_secret,
+        callbackURL: env.base_url + "/auth/google/callback"
+    },
+        function (accessToken, refreshToken, profile, done) {
+            //return done(err, user);
+        }
+    ));
+} else {
+    console.log('define google_client_id and google_client_secret in your env.js file');
+}
 
 router.get('/google',
     passport.authenticate('google', { scope: ['https://www.googleapis.com/auth/plus.login'] }));
@@ -20,9 +26,9 @@ router.get('/google',
 router.get('/google/callback',
     passport.authenticate('google', { failureRedirect: '/login' }),
     function (req, res) {
-        
+
         console.log('Google Callback');
-        
+
         res.redirect('/');
     });
 
