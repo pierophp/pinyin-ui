871d1631f927f103689bf23c5b096b68f4f12d82
Piero Giusti <pierophp@gmail.com>
2016-06-23 14:34:07 -0300

    Fix Client Test Config

--- karma.conf.js
+++ karma.conf.js
@@ -24,8 +24,10 @@ module.exports = function (config) {
       'public/lib/ng-pinyin/ng-pinyin.min.js',
       'public/lib/angular-elastic-input/dist/angular-elastic-input.min.js',
       'public/lib/angular-drag-and-drop-lists/angular-drag-and-drop-lists.min.js',
+      'public/js/directives/*.js',
       'public/js/app.js',
-      'public/js/services/filesAPIService.js',
+      'public/js/filters/*.js',
+      'public/js/services/*.js',
       'tests/client/*.js'
     ],
     
