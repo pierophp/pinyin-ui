a5f11488619d1bfe0349b7119ed8bcf17490b836
Piero Giusti <pierophp@gmail.com>
2016-06-23 18:37:59 -0300

    Added New Relic

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+if (process.env.NODE_ENV == 'production') {
+    require('newrelic');
+}
+
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
@@ -8,6 +12,6 @@ app.use('/auth', require('./controllers/AuthController'));
 app.use('/files', require('./controllers/FilesController'));
 app.use('/unihan', require('./controllers/UnihanController'));
 
-app.listen(9090, function() {
+app.listen(9090, function () {
     console.log('Pinyin app listening on port 9090!');
 });
