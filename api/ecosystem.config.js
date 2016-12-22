b0486457ab583155d20f808e3690e097c64ae4d2
Piero Giusti <pierophp@gmail.com>
2016-12-22 11:00:38 -0200

    Travis change

---
+++ api/ecosystem.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+ /**
+   * Deployment section
+   * http://pm2.keymetrics.io/docs/usage/deployment/
+   */
+  deploy: {
+    production: {
+      user: process.env.SSH_USER,
+      host: [{
+        host: process.env.SSH_HOST,
+        port: process.env.SSH_PORT,
+      },
+      ],
+      ref: 'origin/master',
+      repo: 'git@github.com:pierophp/pinyin.git',
+      path: '/var/www/api.pinyin',
+      'post-deploy': 'cd ui && npm install && npm run build && cd .. && rm -Rf /var/www/pinyin && cp -R ./ui/dist /var/www/pinyin && cd api && cp ../../env/* . && npm install --production && pm2 startOrRestart ecosystem.json --env production',
+    },
+  },
+};
