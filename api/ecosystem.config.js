85cbe4edcb8414e8f63ed0e0b47d631a24a7cbb8
Piero Giusti <pierophp@gmail.com>
2016-12-23 05:03:04 -0200

    Fix deploy

--- api/ecosystem.config.js
+++ api/ecosystem.config.js
@@ -13,6 +13,7 @@ module.exports = {
       ],
       ref: 'origin/master',
       repo: 'git@github.com:pierophp/pinyin.git',
+      ssh_options: 'StrictHostKeyChecking=no',
       path: '/var/www/api.pinyin',
       'post-deploy': 'cd ui && npm install && npm run build && cd .. && rm -Rf /var/www/pinyin && cp -R ./ui/dist /var/www/pinyin && cd api && cp ../../env/* . && npm install --production && pm2 startOrRestart ecosystem.json --env production',
     },
