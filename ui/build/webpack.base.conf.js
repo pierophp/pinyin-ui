fc762a61be535976ccdee3f3b3a7263ce4a09932
Piero Giusti <pierophp@gmail.com>
2017-02-06 15:34:16 -0200

    Fix Noto Sans

--- ui/build/webpack.base.conf.js
+++ ui/build/webpack.base.conf.js
@@ -28,6 +28,7 @@ module.exports = {
       externals: [
         'https://fonts.googleapis.com/css?family=Roboto:300,400,500,700,400italic',
         'https://fonts.googleapis.com/icon?family=Material+Icons',
+        'https://fonts.googleapis.com/earlyaccess/notosanssc.css',
       ],
     }),
   ],
