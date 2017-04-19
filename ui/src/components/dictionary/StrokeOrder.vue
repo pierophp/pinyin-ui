026fd11bf78854137413a456ce7dd31ee6665010
Piero Giusti <pierophp@gmail.com>
2017-04-19 00:18:29 -0300

    Stroke Order

---
+++ ui/src/components/dictionary/StrokeOrder.vue
@@ -0,0 +1,121 @@
+<template>
+  <div class="hanzi-container">
+    <div id="hanzi-controls">
+      <span v-for="(item, itemId) in items" @click="changeIdeogram(itemId)" :class="item.classActive"></span>
+    </div>
+
+    <div id="hanzi-writer"></div>
+    <md-button class="md-icon-button md-raised md-primary" @click.native="animate">
+      <md-icon>play_arrow</md-icon>
+    </md-button>
+  </div>
+</template>
+
+<script>
+  import HanziWriter from 'hanzi-writer';
+
+  export default {
+    name: 'dictionary-stroke-order',
+    watch: {
+      ideograms() {
+        this.update();
+      },
+    },
+    data() {
+      return {
+        items: [],
+      };
+    },
+    mounted() {
+      this.writer = new HanziWriter('hanzi-writer', '我', {
+        showOutline: true,
+        showCharacter: true,
+        width: 250,
+        height: 250,
+        padding: 0,
+        strokeAnimationDuration: 300, // duration of each stroke in ms
+        delayBetweenStrokes: 500, // delay between drawing subsequent strokes in ms
+        strokeColor: '#555',
+        highlightColor: '#AAF', // color used to highlight strokes as a hint during quizzing
+        outlineColor: '#DDD',
+        drawingColor: '#333', // color of the line drawn by the user during quizzing
+
+        showHintAfterMisses: 3, // give a hint after this many subsequent mistakes during quizzing
+        highlightOnComplete: true, // flash the character when the quiz is successfully completed
+      });
+      this.update();
+    },
+    methods: {
+      animate() {
+        this.writer.animateCharacter({
+          onComplete: () => {
+          },
+        });
+      },
+      changeIdeogram(itemId) {
+        this.items.forEach((item, i) => {
+          this.items[i].classActive = '';
+        });
+        this.items[itemId].classActive = 'active';
+        this.writer.setCharacter(this.items[itemId].ideogram);
+      },
+      update() {
+        this.items = [];
+        for (let i = 0; i < this.ideograms.length; i += 1) {
+          if (i === 0) {
+            this.writer.setCharacter(this.ideograms[i]);
+          }
+          const classActive = (i === 0) ? 'active' : '';
+          this.items.push({
+            classActive,
+            ideogram: this.ideograms[i],
+          });
+        }
+      },
+    },
+    props: {
+      ideograms: {
+
+      },
+    },
+  };
+</script>
+
+<style>
+.hanzi-container {
+  width: 250px;
+  text-align: center;
+  position: relative;
+}
+
+#hanzi-writer svg {
+  min-height: 250px;
+}
+
+#hanzi-controls {
+  top: 20px;
+  left: 260px;
+  position: absolute;
+  width: 30px;
+  height: 100px;
+}
+
+#hanzi-controls span{
+  width: 10px;
+  height: 10px;
+  border-radius: 100%;
+  background: #007aff;
+  display: block;
+  margin-bottom: 18px;
+  background: #000;
+  opacity: .2;
+  cursor: pointer;
+  z-index: 1000;
+  transition:all 0.3s ease;
+}
+
+#hanzi-controls span.active{
+  background: #007aff;
+  opacity: 1;
+}
+</style>
--- ui/src/pages/dictionary/Search.vue
+++ ui/src/pages/dictionary/Search.vue
@@ -4,7 +4,7 @@
     <md-input-container>
       <md-icon>pageview</md-icon>
       <label>{{ $t("search") }}</label>
-      <md-input @change="search" type="text" ref="inputSearch" autofocus></md-input>
+      <md-input @change="search" type="text" ref="inputSearch" autofocus v-model="searchValue"></md-input>
     </md-input-container>
   </form>
 
@@ -12,7 +12,16 @@
     <h2 v-if="dictionary.pronunciation">
     {{ dictionary.pronunciation }} <md-icon class="md-warn sound" @click.native="openSound">volume_up</md-icon>
     </h2>
-    <dictionary-list :dictionary="dictionary"/>
+
+    <md-tabs>
+      <md-tab id="dict" md-label="Dict">
+        <dictionary-list :dictionary="dictionary"/>
+      </md-tab>
+
+      <md-tab id="stroke" md-label="Stroke">
+        <dictionary-stroke-order :ideograms="searchValue"/>
+      </md-tab>
+    </md-tabs>
   </loadable-content>
 
   <md-dialog ref="dialogForvo">
@@ -31,19 +40,27 @@
   import http from 'src/helpers/http';
   import LoadableContent from 'src/components/common/loading/LoadableContent';
   import DictionaryList from 'src/components/dictionary/List';
+  import DictionaryStrokeOrder from 'src/components/dictionary/StrokeOrder';
 
   export default {
     name: 'dicionary-search',
     components: {
       LoadableContent,
       DictionaryList,
+      DictionaryStrokeOrder,
     },
     data() {
       return {
+        searchValue: '耶和华',
         forvoUrl: null,
         dictionary: {},
         loading: false,
       };
+    },
+    mounted() {
+      if (this.searchValue) {
+        this.search(this.searchValue);
+      }
     },
     methods: {
       search(value) {
@@ -76,6 +93,19 @@
   flex: 1;
   padding: 0 10px;
   overflow: auto;
+}
+
+.dictionary-container .md-input-container {
+  margin-bottom: 15px;
+}
+
+.dictionary-container h2{
+  margin-top: 0;
+  -webkit-margin-before: 0;
+}
+
+.dictionary-container .md-tabs .md-tab {
+  padding: 10px;
 }
 .sound{
   cursor: pointer;
