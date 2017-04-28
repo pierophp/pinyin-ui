09734056ed9b2b30ed7e43dff5e545354ed3fc09
Piero Giusti <pierophp@gmail.com>
2017-04-28 17:02:54 -0300

    New Fields

---
+++ api/migrations/20170428165139_cjk_main.js
@@ -0,0 +1,10 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.table('cjk', function (table) {
+        table.boolean('main');
+  });
+};
+
+exports.down = function(knex, Promise) {
+
+};
---
+++ api/migrations/20170428165148_user_admin.js
@@ -0,0 +1,10 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.table('user', function (table) {
+        table.boolean('admin');
+  });
+};
+
+exports.down = function(knex, Promise) {
+
+};
