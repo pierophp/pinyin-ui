d2eb5e671a27ca468ab5dc453e9266640860b124
Piero Giusti <pierophp@gmail.com>
2017-05-24 10:29:18 -0300

    Fix deploy

--- api/ecosystem.config.js
+++ api/ecosystem.config.js
@@ -1,8 +1,24 @@
+let postDeploy = `
+        cd /home/ubuntu/ &&
+        rm -Rf dist/ &&
+        unzip pinyin.dist.zip &&
+        sudo rm -Rf /var/www/pinyin &&
+        mv /home/ubuntu/dist /var/www/pinyin &&
+        ln -s /var/local/pinyin/Dicionario_Pleco.txt /var/www/pinyin/Dicionario_Pleco.txt &&
+        cd /var/www/api.pinyin/current/api &&
+        cp ../../env/* . &&
+        yarn install --production &&
+        knex migrate:latest --env production &&
+        sudo pm2 startOrRestart ecosystem.json --env production`;
+
+postDeploy = postDeploy.split('\n').join(' ');
+
 module.exports = {
  /**
    * Deployment section
    * http://pm2.keymetrics.io/docs/usage/deployment/
    */
+
   deploy: {
     production: {
       user: process.env.SSH_USER,
@@ -15,18 +31,7 @@ module.exports = {
       repo: 'https://github.com/pierophp/pinyin.git',
       ssh_options: 'StrictHostKeyChecking=no',
       path: '/var/www/api.pinyin',
-      'post-deploy': `
-        cd /home/ubuntu/ &&
-        rm -Rf dist/ &&
-        unzip pinyin.dist.zip &&
-        sudo rm -Rf /var/www/pinyin &&
-        mv /home/ubuntu/dist /var/www/pinyin &&
-        ln -s /var/local/pinyin/Dicionario_Pleco.txt /var/www/pinyin/Dicionario_Pleco.txt &&
-        cd /var/www/api.pinyin/current/api &&
-        cp ../../env/* . &&
-        yarn install --production &&
-        knex migrate:latest --env production &&
-        sudo pm2 startOrRestart ecosystem.json --env production`,
+      'post-deploy': postDeploy,
     },
   },
 };
