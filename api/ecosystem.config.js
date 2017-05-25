b05f11fffe8e94b957821b1c6d1620a780e4fe4f
Piero Giusti <pierophp@gmail.com>
2017-05-25 10:55:56 -0300

    Fix deploy

--- api/ecosystem.config.js
+++ api/ecosystem.config.js
@@ -1,17 +1,20 @@
-let postDeploy = `
-        cd /home/ubuntu/ &&
-        rm -Rf dist/ &&
-        unzip pinyin.dist.zip &&
-        sudo rm -Rf /var/www/pinyin &&
-        mv /home/ubuntu/dist /var/www/pinyin &&
-        ln -s /var/local/pinyin/Dicionario_Pleco.txt /var/www/pinyin/Dicionario_Pleco.txt &&
-        cd /var/www/api.pinyin/current/api &&
-        cp ../../env/* . &&
-        yarn install --production &&
-        knex migrate:latest --env production &&
-        sudo pm2 startOrRestart ecosystem.json --env production`;
+const postDeployCommands = [
+  'cd /home/ubuntu/',
+  'rm -Rf dist/',
+  'unzip pinyin.dist.zip',
+  'sudo rm -Rf /var/www/pinyin',
+  'mv /home/ubuntu/dist /var/www/pinyin',
+  'ln -s /var/local/pinyin/Dicionario_Pleco.txt /var/www/pinyin/Dicionario_Pleco.txt',
+  'cd /var/www/api.pinyin/current/api',
+  'cp ../../env/* .',
+  'yarn install --production',
+  'knex migrate:latest --env production',
+  'sudo pm2 startOrRestart ecosystem.json --env production',
+];
 
-postDeploy = postDeploy.split('\n').join(' ');
+const postDeploy = postDeployCommands.join(' && ');
+
+console.log(postDeploy);
 
 module.exports = {
  /**
