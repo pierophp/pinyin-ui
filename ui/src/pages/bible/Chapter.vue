6cd1af6dbe3e7e60220f94236db60d399feb29d2
Piero Giusti <pierophp@gmail.com>
2017-10-12 03:58:55 -0300

    File Loading

--- ui/src/pages/bible/Chapter.vue
+++ ui/src/pages/bible/Chapter.vue
@@ -1,39 +1,48 @@
 <template>
-  <div class="bible-chapter-container">
-    <file-print/>
-  </div>
+  <file-print/>
 </template>
 
 <script>
   import axios from 'axios';
   import FilePrint from 'src/pages/files/FilePrint';
+  import LoadableContent from 'src/components/common/loading/LoadableContent';
   import {
     mapMutations,
   } from 'vuex';
 
   import {
     FILE_MUTATION_SET,
+    FILE_MUTATION_SET_FILE_LOADING,
   } from 'src/data/file/types';
 
   export default {
     name: 'bible-chapter',
     components: {
       FilePrint,
+      LoadableContent,
+    },
+    data() {
+      return {
+        loading: true,
+      };
     },
     methods: {
       ...mapMutations({
         setFileContent: FILE_MUTATION_SET,
+        setFileLoading: FILE_MUTATION_SET_FILE_LOADING,
       }),
     },
     async created() {
+      this.setFileLoading(true);
       const content = await axios.get(`static/bible/cmn-hans/${this.$route.params.book}/${this.$route.params.chapter}.json`);
       this.setFileContent({ file: content.data.lines });
+      this.loading = false;
     },
   };
 </script>
 
 <style>
 .bible-chapter-container {
-  overflow: auto;
+
 }
 </style>
