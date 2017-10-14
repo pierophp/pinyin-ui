d4ede2880bc6f7fcf65e1d774fa3fbf9af3e4cc2
Piero Giusti <pierophp@gmail.com>
2017-10-14 11:44:27 -0300

    Fix Bible

--- ui/src/pages/bible/Chapter.vue
+++ ui/src/pages/bible/Chapter.vue
@@ -1,8 +1,17 @@
 <template>
-  <file-print/>
+  <span class="bible-chapter-container">
+    <div class="verses-container" v-show="showVerses">
+      <div v-for="(verse, verseId) in verses" v-bind:key="verseId" class="bible-verse" @click="selectVerse(verse)">
+          {{ verse }}
+      </div>
+    </div>
+
+    <file-print/>
+  </span>
 </template>
 
 <script>
+  import chaptersData from 'src/data/bible/chapters';
   import axios from 'axios';
   import FilePrint from 'src/pages/files/FilePrint';
   import LoadableContent from 'src/components/common/loading/LoadableContent';
@@ -23,6 +32,12 @@
       FilePrint,
       LoadableContent,
     },
+    data() {
+      return {
+        showVerses: false,
+        verses: [],
+      };
+    },
     methods: {
       ...mapMutations({
         setFileContent: FILE_MUTATION_SET,
@@ -30,6 +45,9 @@
         setFullFile: FILE_MUTATION_SET_FULL_FILE,
         setFileLoading: FILE_MUTATION_SET_FILE_LOADING,
       }),
+      selectVerse() {
+
+      },
       loadFile(lines, lineIndex) {
         if (lines.length === lineIndex) {
           this.setFileLoading(false);
@@ -55,12 +73,57 @@
     },
     async created() {
       this.setFileLoading(true);
-      // this.setFileContent({ file: [] });
+      this.setFileContent({ file: [] });
+      const CACHE_VERSION = 1;
 
-      const content = await axios.get(`static/bible/cmn-hans/${this.$route.params.book}/${this.$route.params.chapter}.json`);
+      axios.get(`static/bible/cmn-hans/${this.$route.params.book}/${this.$route.params.chapter}.json?v=${CACHE_VERSION}`)
+        .then((content) => {
+          this.setFullFile({ file: content.data.lines });
+          this.loadFile(content.data.lines, 0);
+        });
 
-      this.setFullFile({ file: content.data.lines });
-      this.loadFile(content.data.lines, 0);
+      const chapter = chaptersData[this.$route.params.book][this.$route.params.chapter - 1];
+      let initial = 1;
+      if (chapter.i) {
+        initial = chapter.i;
+      }
+
+      for (let i = initial; i <= chapter.t; i += 1) {
+        this.verses.push(i);
+      }
     },
   };
 </script>
+
+<style>
+.bible-chapter-container {
+  display: flex;
+  flex-direction: column;
+  flex-wrap: wrap;
+  align-content: flex-start;
+  width:100%;
+  overflow: auto;
+}
+
+.verses-container {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  align-content: flex-start;
+  width: 100%;
+  margin: 25px;
+}
+
+.bible-verse {
+  color: #fff;
+  line-height: 50px;
+  height: 50px;
+  width: 50px;
+  background-color: #275197;
+  justify-content: flex-start;
+  margin: 2px;
+  text-align: center;
+  font-size: 16px;
+  cursor: pointer;
+}
+</style>
