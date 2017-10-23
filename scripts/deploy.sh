b3acdeda50c0fc353359bb62b3753204bf578cbd
Piero Giusti <pierophp@gmail.com>
2017-10-23 17:34:26 -0200

    Fix deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,9 +1,9 @@
+echo "Deploy Start"
 export SSH_KEY="/home/travis/.ssh/id_rsa"
 export CMD="scp -o StrictHostKeyChecking=no /home/travis/pinyin.dist.zip ${SSH_USER}@${SSH_HOST}:~"
 git checkout -- yarn.lock
 $CMD
 export CMD="scp -o StrictHostKeyChecking=no /home/travis/bible.pinyin.dist.zip ${SSH_USER}@${SSH_HOST}:~"
-git checkout -- yarn.lock
 $CMD
 export CMD="pm2 deploy ecosystem.config.js production"
 $CMD
