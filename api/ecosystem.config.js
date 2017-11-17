8c467433702a861a7c50a73a0a08b0de3cb2a7c7
Piero Giusti <pierophp@gmail.com>
2017-11-17 12:01:52 -0200

    Fix master deploy

--- api/ecosystem.config.js
+++ api/ecosystem.config.js
@@ -2,8 +2,8 @@ const postDeployCommands = [
   'cd /home/ubuntu/',
   // Pinyin Editor UI
   'rm -Rf dist/',
-  'unzip pinyin.dist.zip',
-  'BRANCH=`cat dist/branch.txt`',
+  'unzip pinyin.dist.zip > /dev/null',
+  'BRANCH="$(cat dist/branch.txt)"',
   'echo $BRANCH',
   '[[ $BRANCH = "master" ]] && UI_PATH="/var/www/pinyin" || UI_PATH="/var/www/pinyin.dev"',
   'echo $UI_PATH',
@@ -12,17 +12,17 @@ const postDeployCommands = [
   'ln -s /var/local/pinyin/Dicionario_Pleco.txt /var/www/pinyin/Dicionario_Pleco.txt',
   // Bible Editor UI
   'rm -Rf dist/',
-  'unzip bible.pinyin.dist.zip',
+  'unzip bible.pinyin.dist.zip > /dev/null',
   'sudo rm -Rf /var/www/bible.pinyin',
   'mv /home/ubuntu/dist /var/www/bible.pinyin',
   // Dictionary Editor UI
   'rm -Rf dist/',
-  'unzip dictionary.pinyin.dist.zip',
+  'unzip dictionary.pinyin.dist.zip > /dev/null',
   'sudo rm -Rf /var/www/dictionary.pinyin',
   'mv /home/ubuntu/dist /var/www/dictionary.pinyin',
   // Videos Editor UI
   'rm -Rf dist/',
-  'unzip videos.pinyin.dist.zip',
+  'unzip videos.pinyin.dist.zip > /dev/null',
   'sudo rm -Rf /var/www/videos.pinyin',
   'mv /home/ubuntu/dist /var/www/videos.pinyin',
   // API
