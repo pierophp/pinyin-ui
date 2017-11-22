8a4c39038decd9da1fec1764ad1e85802056a71f
Piero Giusti <pierophp@gmail.com>
2017-11-21 23:53:29 -0200

    Fix deploy staging

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,7 +3,6 @@ cd api/
 echo "Deploy Starting: $DEPLOY_ENV"
 export SSH_KEY="/home/travis/.ssh/id_rsa"
 export CMD="scp -o StrictHostKeyChecking=no /home/travis/pinyin.dist.zip ${SSH_USER}@${SSH_HOST}:~"
-git checkout -- yarn.lock
 $CMD
 export CMD="scp -o StrictHostKeyChecking=no /home/travis/bible.pinyin.dist.zip ${SSH_USER}@${SSH_HOST}:~"
 $CMD
@@ -11,5 +10,5 @@ export CMD="scp -o StrictHostKeyChecking=no /home/travis/dictionary.pinyin.dist.
 $CMD
 export CMD="scp -o StrictHostKeyChecking=no /home/travis/videos.pinyin.dist.zip ${SSH_USER}@${SSH_HOST}:~"
 $CMD
-export CMD="pm2 deploy ecosystem.config.js $DEPLOY_ENV"
+export CMD="pm2 deploy ecosystem.config.js $DEPLOY_ENV --force"
 $CMD
