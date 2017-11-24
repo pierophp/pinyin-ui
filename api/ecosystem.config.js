21e25b89ce679e0f755a99820e9ee646b8b81f74
Piero Giusti <pierophp@gmail.com>
2017-11-23 23:33:14 -0200

    Ecosystem staging

--- api/ecosystem.config.js
+++ api/ecosystem.config.js
@@ -29,29 +29,12 @@ const postDeployCommands = [
   'cp ../../env/* .',
   'yarn install --production',
   'knex migrate:latest --env production',
-  `sudo pm2 startOrRestart ecosystem.json --env ${isProduction ? 'production' : 'staging'}`,
+  `sudo pm2 startOrRestart ecosystem-${isProduction ? 'master' : 'staging'}.json --env production`,
 ];
 
 const postDeploy = postDeployCommands.join(' && ');
 
 module.exports = {
-  apps: [
-    {
-      name: 'Pinyin ' + process.env.TRAVIS_BRANCH,
-      script: 'server.js',
-      env: {
-        COMMON_VARIABLE: 'true',
-      },
-      env_production: {
-        PORT: 3001,
-        NODE_ENV: 'production',
-      },
-      env_staging: {
-        PORT: 3002,
-        NODE_ENV: 'production',
-      },
-    },
-  ],
  /**
    * Deployment section
    * http://pm2.keymetrics.io/docs/usage/deployment/
