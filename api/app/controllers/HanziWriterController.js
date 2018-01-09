cd2cf2a15ac535d8220ce0e4c8b36b1e48a0f288
Piero Giusti <pierophp@gmail.com>
2018-01-09 15:26:16 -0200

    Hanzi Writer fix

--- api/app/controllers/HanziWriterController.js
+++ api/app/controllers/HanziWriterController.js
@@ -4,20 +4,18 @@ const axios = require('axios');
 // eslint-disable-next-line new-cap
 const router = express.Router();
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   const char = req.query.ideogram;
-  axios
-    .get(
-      `http://chanind.github.io/hanzi-writer/cdn/2/data/${encodeURIComponent(
-        char,
-      )}.json`,
-    )
-    .then(response => {
-      res.send({ status: 200, response: response.data });
-    })
-    .catch(e => {
-      res.send({ status: 500, error: e.message });
-    });
+  const url = `https://chanind.github.io/hanzi-writer/cdn/2/data/${encodeURIComponent(
+    char,
+  )}.json`;
+
+  try {
+    const response = await axios.get(url);
+    res.send({ status: 200, response: response.data });
+  } catch (e) {
+    res.send({ status: 500, error: e.message });
+  }
 });
 
 module.exports = router;
