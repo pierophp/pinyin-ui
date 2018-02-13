dc3efdd7efd495c08c9f8e52e3d7734db9757151
Piero Giusti <pierophp@gmail.com>
2018-02-13 17:27:12 -0200

    Add videos tutoriais

--- ui/src/app/videos.js
+++ ui/src/app/videos.js
@@ -1,4 +1,30 @@
 export default {
-  showMenu: false,
+  showMenu: true,
   title: 'app.videos',
+  menu: [
+    {
+      icon: 'play_circle_outline',
+      title: 'menu.video',
+      action: 'goTo',
+      link: '/video',
+    },
+    {
+      icon: 'settings',
+      title: 'menu.settings',
+      action: 'goTo',
+      link: '/config',
+    },
+    {
+      icon: 'help',
+      title: 'menu.about',
+      action: 'goTo',
+      link: '/about',
+    },
+    {
+      icon: 'power_settings_new',
+      title: 'menu.logout',
+      action: 'logout',
+      link: '',
+    },
+  ],
 };
