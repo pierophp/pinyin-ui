c6f83cab855c5e0efd4d11377714b84e5de67647
Piero Giusti <pierophp@gmail.com>
2018-02-28 00:03:01 -0300

    Update cjk

--- api/sql/ccedict.sql
+++ api/sql/ccedict.sql
@@ -17,30 +17,76 @@ WHERE ce.ideogram IS NULL
   AND c.definition_unihan IS NULL
   AND c.definition_pt IS NULL;
 
+-- UPDATE PINYIN
 
-SELECT 
-  ce.ideogram,
-  ce.ideogram_raw,
-  ce.pronunciation ce_pronunciation,
-  c.pronunciation c_pronunciation,
-  ce.definition ce_definition,
-  c.definition_cedict c_definition
-FROM tmp_cedict ce
+UPDATE tmp_cedict ce
+JOIN cjk c 
+  ON c.ideogram = ce.ideogram
+ AND c.pronunciation = ce.pronunciation
+LEFT JOIN cjk c2 
+  ON c2.ideogram = ce.ideogram
+ AND c2.pronunciation = ce.pronunciation_case
+SET c.pronunciation = ce.pronunciation_case
+WHERE c2.id IS NULL
+AND ce.simplified = 1
+AND ce.id NOT IN(175173, 175175, 212941)
+AND ce.ideogram_raw NOT IN('乐'); 
+
+-- MAIN UPDATE
+
+UPDATE tmp_cedict ce
 JOIN cjk c 
   ON c.ideogram = ce.ideogram
  AND c.pronunciation = ce.pronunciation_case
-WHERE -- IFNULL(c.definition_cedict, '') != ce.definition
-  -- AND 
-  ce.ideogram_raw = '王';
+ AND c.simplified = ce.simplified
+SET c.pronunciation_spaced = ce.pronunciation_spaced,
+    c.pronunciation_taiwan = ce.pronunciation_taiwan,
+    c.erhua = ce.erhua,
+    c.measure_words = ce.measure_words,
+    c.variants = ce.variants,
+    c.traditional = ce.traditional,
+    c.definition_cedict = ce.definition;
 
-SELECT * 
-FROM tmp_cedict
-WHERE ideogram_raw = '鮌';
+-- INSERT NEW
 
-SELECT * 
-FROM cjk
-WHERE ideogram_raw = '王';
-
-SELECT * 
-FROM tmp_cedict
-WHERE measure_words != '[]';
+INSERT cjk
+SELECT 
+NULL id,
+ce.ideogram,
+ce.ideogram_raw,
+ce.pronunciation_case pronunciation,
+ce.pronunciation_unaccented,
+ce.pronunciation_spaced,
+ce.pronunciation_taiwan,
+NULL definition_unihan,
+NULL frequency,
+NULL `usage`,
+1 language_id,
+NOW() created_at,
+NULL updated_at,
+'W' type,
+ce.definition definition_cedict,
+NULL definition_pt,
+ce.measure_words,
+ce.simplified,
+ce.traditional,
+ce.variants,
+NULL definition_ct_pt,
+NULL definition_ct_es,
+NULL definition_ct_en,
+NULL hsk,
+0 main,
+ce.erhua,
+NULL definition_glosbe_pt,
+NULL definition_glosbe_es,
+NULL definition_glosbe_en,
+NULL definition_2pinyin_pt,
+NULL definition_2pinyin_es,
+NULL definition_2pinyin_en  
+FROM tmp_cedict ce
+LEFT JOIN cjk c 
+  ON c.ideogram = ce.ideogram
+ AND c.pronunciation = ce.pronunciation_case
+ AND c.simplified = ce.simplified
+WHERE c.id IS NULL
+LIMIT 100000000;
