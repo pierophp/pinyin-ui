4e1b0682f397af9c411f8e2ceceb8f2b3f6e7ada
Piero Giusti <pierophp@gmail.com>
2018-03-20 10:58:44 -0300

    CjkRepository

--- api/app/repository/base.repository.ts
+++ api/app/repository/base.repository.ts
@@ -1,1 +1,11 @@
-export class BaseRepository {}
+import { RepositoryManager } from './database/repository.manager';
+
+export class BaseRepository {
+  static async getTransaction() {
+    return await RepositoryManager.getTransaction();
+  }
+
+  static async getMysqlConnection() {
+    return await RepositoryManager.getMysqlConnection();
+  }
+}
--- api/app/repository/cjk.repository.ts
+++ api/app/repository/cjk.repository.ts
@@ -1,46 +1,186 @@
+import * as bluebird from 'bluebird';
+import * as replaceall from 'replaceall';
 import { BaseRepository } from './base.repository';
-import { CjkRepository as DatabaseCjkRepository } from './database/cjk.repository';
+import { LanguageRepository } from './language.repository';
+import { PhraseRepository } from './phrase.repository';
+import * as knex from '../services/knex';
+import * as UnihanSearch from '../services/UnihanSearch';
+import * as profiler from '../helpers/profiler';
+
+import { ElasticsearchProvider } from '../core/search/elasticsearch.provider';
 
 export class CjkRepository extends BaseRepository {
   static async findAll() {
-    return DatabaseCjkRepository.findAll();
+    return await knex('cjk')
+      .where({})
+      // .limit(10)
+      .orderBy('frequency', 'ASC')
+      .select();
+  }
+
+  static async findPtNotNull(): Promise<any[]> {
+    return await knex('cjk')
+      .whereRaw(
+        `definition_pt IS NOT NULL AND simplified = 1 AND (type = "W" OR (type = "C" AND frequency < 999))`,
+      )
+      .select();
   }
 
   static async findChineseToolsNotNull(language: string): Promise<any[]> {
-    return DatabaseCjkRepository.findChineseToolsNotNull(language);
+    return await knex('cjk')
+      .whereRaw(
+        `definition_ct_${language} IS NOT NULL AND simplified = 1 AND (type = "W" OR (type = "C" AND frequency < 999))`,
+      )
+      // .limit(10)
+      .select();
   }
 
   static async findChineseToolsIsNull(
     language: string,
     limit: number,
   ): Promise<any[]> {
-    return DatabaseCjkRepository.findChineseToolsIsNull(language, limit);
+    return await knex('cjk')
+      .whereRaw(
+        `definition_ct_${language} IS NULL AND simplified = 1 AND (type = "W" OR (type = "C" AND frequency < 999))`,
+      )
+      .limit(limit)
+      .orderBy('hsk', 'ASC')
+      .orderBy('usage', 'DESC')
+      .select();
   }
 
   static async findGlosbeNotNull(language: string): Promise<any[]> {
-    return DatabaseCjkRepository.findGlosbeNotNull(language);
+    return await knex('cjk')
+      .whereRaw(
+        `definition_glosbe_${language} IS NOT NULL AND simplified = 1 AND (type = "W" OR (type = "C" AND frequency < 999))`,
+      )
+      // .limit(10)
+      .select();
   }
 
   static async findGlosbeIsNull(
     language: string,
     limit: number,
   ): Promise<any[]> {
-    return DatabaseCjkRepository.findGlosbeIsNull(language, limit);
+    return await knex('cjk')
+      .whereRaw(
+        `definition_glosbe_${language} IS NULL AND simplified = 1 AND (type = "W" OR (type = "C" AND frequency < 999))`,
+      )
+      .limit(limit)
+      .orderBy('hsk', 'ASC')
+      .orderBy('usage', 'DESC')
+      .select();
   }
 
   static async findIdeogramRawIsNull(): Promise<any[]> {
-    return DatabaseCjkRepository.findIdeogramRawIsNull();
+    return await knex('cjk')
+      .whereRaw(`ideogram_raw IS NULL`)
+      .select();
   }
 
-  static async searchPronunciationByWord(ideograms) {
-    return DatabaseCjkRepository.searchPronunciationByWord(ideograms);
+  static async searchPronunciationByWord(ideograms: string) {
+    const ideogramConverted = UnihanSearch.convertIdeogramsToUtf16(ideograms);
+    let response: any = null;
+    if (ideograms.length === 1) {
+      response = await knex('cjk')
+        .where({
+          ideogram: ideogramConverted,
+          type: 'C',
+        })
+        .orderBy('frequency', 'ASC')
+        .orderBy('usage', 'DESC')
+        .select('id', 'pronunciation');
+
+      if (response.length > 0) {
+        return response[0].pronunciation;
+      }
+    }
+
+    response = await knex('cjk')
+      .where({
+        ideogram: ideogramConverted,
+        type: 'W',
+      })
+      .orderBy('frequency', 'ASC')
+      .orderBy('usage', 'DESC')
+      .select('id', 'pronunciation');
+
+    if (response.length > 0) {
+      return response[0].pronunciation;
+    }
+
+    return null;
   }
 
-  static async save(cjk) {
-    return DatabaseCjkRepository.save(cjk);
+  static async save(params) {
+    // let action = 'insert';
+
+    if (params.id) {
+      params.updated_at = new Date();
+
+      await knex('cjk')
+        .where('id', '=', params.id)
+        .update(params);
+    } else {
+      params.created_at = new Date();
+
+      params.id = (await knex('cjk')
+        .insert(params)
+        .returning('id'))[0];
+    }
+
+    const cjk = (await knex('cjk').where({
+      id: params.id,
+    }))[0];
+
+    const elasticsearchProvider = new ElasticsearchProvider();
+
+    await elasticsearchProvider.saveMany([cjk]);
   }
 
   static async referencePhrases() {
-    return DatabaseCjkRepository.referencePhrases();
+    const words = await knex('cjk')
+      .where({
+        type: 'W',
+        simplified: 1,
+      })
+      .select('id', 'ideogram');
+
+    const characters = await knex('cjk')
+      .whereRaw('type = "C" AND frequency < 999 AND simplified = 1')
+      .select('id', 'ideogram');
+
+    const items = words.concat(characters);
+
+    const language = await LanguageRepository.findOneByCode('cmn-hans');
+
+    let i = 0;
+
+    await bluebird.mapSeries(items, async (item: any) => {
+      let ideograms = UnihanSearch.convertUtf16ToIdeograms(item.ideogram);
+      ideograms = replaceall('%', '', ideograms);
+      if (!ideograms) {
+        return;
+      }
+
+      i += 1;
+
+      profiler(`${i} - ${ideograms}`);
+
+      const phrases = await PhraseRepository.findByLanguageAndRlike(
+        language,
+        ideograms,
+      );
+      if (phrases.length === 0) {
+        return;
+      }
+
+      await bluebird.mapSeries(phrases, async (phrase: any) => {
+        await knex('cjk_has_phrase').insert({
+          cjk_id: item.id,
+          phrase_id: phrase.id,
+        });
+      });
+    });
   }
 }
