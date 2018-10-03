73caa4d2c97ed069d0ce06530fc82939a47c5746
Piero Giusti <pierophp@gmail.com>
2018-10-03 11:59:24 +0800

    Fix webvtt parser

--- ui/src/domain/webvtt-parser.js
+++ ui/src/domain/webvtt-parser.js
@@ -1,13 +1,16 @@
-export default function (lines) {
+export default function(lines) {
   let i = 0;
   const tracks = [];
-  lines.forEach((line) => {
+  lines.forEach(line => {
     const lineSplit = line.split('-->');
     if (lineSplit.length > 1) {
       i += 1;
+
+      const endTime = lineSplit[1].trim().split(' ');
+
       tracks[i] = {};
       tracks[i].startTime = lineSplit[0].trim();
-      tracks[i].endTime = lineSplit[1].trim();
+      tracks[i].endTime = endTime[0];
       tracks[i].message = [];
       return;
     }
