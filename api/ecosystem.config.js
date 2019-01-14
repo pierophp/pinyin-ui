c709863b6ccf759e5f809b2a4a83954cee4a2903
Piero Giusti <pierophp@gmail.com>
2019-01-14 15:07:35 +0800

    Fix deploy

--- api/ecosystem.config.js
+++ api/ecosystem.config.js
@@ -52,7 +52,7 @@ const postDeployCommands = [
   }`,
   '[ -d ../../source/api/dist/ ] && cp -R ../../source/api/app/data ../../source/api/dist/api/app/data',
   'cp ../../env/* .',
-  'git submodule init',
+  'git submodule update',
   'yarn install --production',
   'yarn build',
   'knex migrate:latest --env production',
