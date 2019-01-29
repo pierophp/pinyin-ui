ab30cf23f6c718950d68b54a0877420530accd0f
Piero Giusti <pierophp@gmail.com>
2019-01-30 00:40:14 +0800

    Remove file not used

--- ui/test/unit/specs/SeparatePinyinInSyllables.spec.js
+++
@@ -1,7 +0,0 @@
-import separatePinyinInSyllables from 'shared/helpers/separate-pinyin-in-syllables';
-describe('Separate pinyin in syllables', () => {
-  it('Separate pinyin in syllables', () => {
-    expect(separatePinyinInSyllables('jiānrěnbùbá'))
-      .to.equal('jiān rěn bù bá');
-  });
-});
