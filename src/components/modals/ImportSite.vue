12dfe4f7174a1f3fd7e586b9d59a110d55c0040a
Piero Giusti <pierophp@gmail.com>
2019-11-09 13:13:43 -0200

    Fix inport site

--- src/components/modals/ImportSite.vue
+++ src/components/modals/ImportSite.vue
@@ -1,5 +1,5 @@
 <template>
-  <md-dialog ref="modal" :md-active.sync="modalOpen" :md-fullscreen="false">
+  <md-dialog ref="modal" :md-active.sync="modalOpen" :md-fullscreen="true">
     <md-dialog-title>{{ $t('import_site') }}</md-dialog-title>
 
     <md-dialog-content>
